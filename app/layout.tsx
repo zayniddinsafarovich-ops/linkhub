a8afbb385aeda2fb7fe3e4b019f69d06277178f6
zayniddinsafarovich-ops <zayniddinsafarovich@gmail.com>
2026-05-01 16:52:17 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,3 @@
-import "./globals.css"; export default function RootLayout({ children }: any) { return <html><body>{children}</body>body></html>html></body></>html>}</html>
+import "./globals.css";
+export default function RootLayout({ children }: any) {
+  return ( <html> <body> {children} </body>body> </html>html> ); }</html>
