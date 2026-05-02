b7a56c30f33dc655343136e63ee5b7bd2b384f22
zayniddinsafarovich-ops <zayniddinsafarovich@gmail.com>
2026-05-02 10:41:44 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-          domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-    },
-};
 
-export default nextConfig;
